496661436db61e4c94c160563bc89cc5ba4a34ba
Trevor Jameson <tjameson15@gmail.com>
2018-07-19 11:30:42 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
+def my_collect(arr)
+  arr_clone = []
+  counter = 0
+  while counter < arr.length
+    arr_clone << yield(arr[counter])
+    counter += 1
+  end
+  return arr_clone
+end
 
-
+# test_data = ["Tim Jones", "Tom Smith", "Sophie Johnson", "Antoin Miller"]
+# my_collect(test_data) {|i| puts i.split(" ")}
